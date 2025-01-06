d6b8cb2cf63283d442079549b5c8a6187a226113
Shafiul islam mashruf <105199738+mashruf99@users.noreply.github.com>
2025-01-06 23:23:01 +0600

    3

---
+++ Week _7/Day_5/B_Fedor_and_New_Game.cpp
@@ -0,0 +1,65 @@
+// MASHRUF's work
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define ff first
+#define mp make_pair
+#define no cout << "NO" << '\n'
+#define yes cout << "YES" << '\n'
+#define endl '\n'
+#define fast() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define mod 1000000007
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define sz(x) (int)x.size()
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vi vector<int>
+#define vll vector<ll>
+#define vvi vector<vector<int>>
+#define vvl vector<vector<ll>>
+#define vpii vector<pair<int, int>>
+#define vpll vector<pair<ll, ll>>
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define rrep(i, a, b) for (int i = a; i >= b; i--)
+#define rrep(i, a, b) for (int i = a; i >= b; i--)
+#define rrep(i, a, b) for (int i = a; i >= b; i--)
+#define each(x, a) for (auto &x : a)
+#define lb lower_bound
+#define ub upper_bound
+#define bs binary_search
+#define mem(a, b) memset(a, b, sizeof(a))
+#define dbg(x) cout << #x << " = " << x << '\n'
+#define dbg2(x, y) cout << #x << " = " << x << ", " << #y << " = " << y << '\n'
+#define dbg3(x, y, z) cout << #x << " = " << x << ", " << #y << " = " << y << ", " << #z << " = " << z << '\n'
+#define dbg4(w, x, y, z) cout << #w << " = " << w << ", " << #x << " = " << x << ", " << #y << " = " << y << ", " << #z << " = " << z << '\n'
+#define dbg5(v, w, x, y, z) cout << #v << " = " << v << ", " << #w << " = " << w << ", " << #x << " = " << x << ", " << #y << " = " << y << ", " << #z << " = " << z << '\n'
+#define dbg6(u, v, w, x, y, z) cout << #u << " = " << u << ", " << #v << " = " << v << ", " << #w << " = " << w << ", " << #x << " = " << x << ", " << #y << " = " << y << ", " << #z << " = " << z << '\n'
+
+
+
+int main()
+{
+    fast();
+    int n, m, k;
+    cin >> n >> m >> k;
+    vi a(m + 1);
+    rep(i, 0, m + 1) cin >> a[i];
+    int ans = 0;
+    rep(i, 0, m)
+    {
+        int cnt = 0;
+        rep(j, 0, n)
+        {
+            if (((a[i] >> j & 1) != (a[m] >> j & 1)))
+                cnt++;
+        }
+        if (cnt <= k)
+            ans++;
+    }
+    cout << ans << endl;
+
+    return 0;
+}
