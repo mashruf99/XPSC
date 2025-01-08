b5b69290ac74bc44629b2595091753c5c225aa17
Shafiul islam mashruf <105199738+mashruf99@users.noreply.github.com>
2025-01-08 23:43:52 +0600

    3

---
+++ Week _7/Day_7/B_XOR_Palindromes.cpp
@@ -0,0 +1,93 @@
+// MASHRUF's work
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define ff first
+#define mp make_pair
+#define no cout << "NO" << '\n'
+#define yes cout << "YES" << '\n'
+#define endl '\n'
+#define fast() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define mod 1000000007
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define sz(x) (int)x.size()
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vi vector<int>
+#define vll vector<ll>
+#define vvi vector<vector<int>>
+#define vvl vector<vector<ll>>
+#define vpii vector<pair<int, int>>
+#define vpll vector<pair<ll, ll>>
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define rrep(i, a, b) for (int i = a; i >= b; i--)
+#define rrep(i, a, b) for (int i = a; i >= b; i--)
+#define rrep(i, a, b) for (int i = a; i >= b; i--)
+#define each(x, a) for (auto &x : a)
+#define lb lower_bound
+#define ub upper_bound
+#define bs binary_search
+#define mem(a, b) memset(a, b, sizeof(a))
+#define dbg(x) cout << #x << " = " << x << '\n'
+#define dbg2(x, y) cout << #x << " = " << x << ", " << #y << " = " << y << '\n'
+#define dbg3(x, y, z) cout << #x << " = " << x << ", " << #y << " = " << y << ", " << #z << " = " << z << '\n'
+#define dbg4(w, x, y, z) cout << #w << " = " << w << ", " << #x << " = " << x << ", " << #y << " = " << y << ", " << #z << " = " << z << '\n'
+#define dbg5(v, w, x, y, z) cout << #v << " = " << v << ", " << #w << " = " << w << ", " << #x << " = " << x << ", " << #y << " = " << y << ", " << #z << " = " << z << '\n'
+#define dbg6(u, v, w, x, y, z) cout << #u << " = " << u << ", " << #v << " = " << v << ", " << #w << " = " << w << ", " << #x << " = " << x << ", " << #y << " = " << y << ", " << #z << " = " << z << '\n'
+
+void ok()
+{
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    string ans= "";
+    int goodpair=0,lack=0,start = 0, last = n-1;
+    while(start<last){
+        if(s[start]==s[last]){
+            goodpair+=2;
+        }
+        else{
+            lack++;
+        }
+        start++;
+        last--;
+    }
+
+    for(int i = 0; i <= n; i++){
+        int total = i;
+        total-=lack;
+
+        if(total< 0){
+            ans.push_back('0');
+            continue;
+        }
+        total = max((total%2),total-goodpair);
+        total = max(0,total-(n%2));
+
+        if(total == 0){
+            ans.push_back('1');
+        }
+        else{
+            ans.push_back('0');
+        }
+    }
+    cout << ans << endl;
+    
+}
+
+int main()
+{
+    fast();
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        ok();
+    }
+
+    return 0;
+}
